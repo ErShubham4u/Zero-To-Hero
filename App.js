2c4ad0a738b63bfe270c6482f43578bd5916be74
ErShubham4u <connect2ershubham@gmail.com>
2026-01-17 12:00:00 +0530

    Class - 01 ( Part - 05 )

--- App.js
+++ App.js
@@ -61,6 +61,20 @@ ReactElement(Object) => HTML(Browser Understands)
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(parent);
 
+
+/*
+<div id="parent">
+  <div id="child">
+    <h1>I'm h1 tag</h1>
+    <h2>I'm h2 tag</h2>
+  </div>
+  <div id="child">
+    <h1>I'm h1 tag</h1>
+    <h2>I'm h2 tag</h2>
+  </div>
+</div>;
+*/
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
@@ -74,8 +88,12 @@ const parent = React.createElement("div", { id: "parent" }, [
 
 // JSX
 
+// console.log(parent); // object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
 console.log(parent); // object
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("heading"));
 root.render(parent);
 
 
